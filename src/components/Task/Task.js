94322408d5191e2f6e182df7912792be725f456b
Souptik Sarkar <souptiksarkar@Souptiks-MacBook-Air.local>
2021-03-12 12:22:33 +0530

    adjusted styling

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -10,7 +10,7 @@ class Task extends Component {
 		this.state = {
 			newTodo: '', // keeping track of new todo
 			taskText: this.props.text,
-			isGettingEdited: false // based on this value a form will show up to edit the task title
+			isGettingEdited: false, // based on this value a form will show up to edit the task title
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,48 +21,68 @@ class Task extends Component {
 		this.onDragOver = this.onDragOver.bind(this);
 		this.handleOnDrop = this.handleOnDrop.bind(this);
 	}
-	handleChange(event) { // handling change for input form of new todo
+	handleChange(event) {
+		// handling change for input form of new todo
 		this.setState({ newTodo: event.target.value });
 	}
-	handleSubmit(event) { // handling submit for input form of new todo
+	handleSubmit(event) {
+		// handling submit for input form of new todo
 		event.preventDefault();
 		const newTodoObject = {
 			id: uuid(),
-			text: this.state.newTodo
+			text: this.state.newTodo,
 		};
 		this.props.addTodoToTask(this.props.id, newTodoObject);
 		this.setState({ newTodo: '' });
 	}
-	toggleEditForm() { // to toggle the isGettingEdited state and enable option for editing the title of the task
+	toggleEditForm() {
+		// to toggle the isGettingEdited state and enable option for editing the title of the task
 		this.setState({ isGettingEdited: !this.state.isGettingEdited });
 	}
-	handleEditSubmit(event) { // handling submit for edit form of existing task
+	handleEditSubmit(event) {
+		// handling submit for edit form of existing task
 		event.preventDefault();
 		this.props.editTask(this.props.id, this.state.taskText);
 		this.toggleEditForm();
 	}
-	handleEditChange(event) { // handling change for edit form of existing task
+	handleEditChange(event) {
+		// handling change for edit form of existing task
 		this.setState({ taskText: event.target.value });
 	}
-	handleTaskDelete() { // handling delete option for existing task
+	handleTaskDelete() {
+		// handling delete option for existing task
 		this.props.deleteTask(this.props.id);
 	}
 	onDragOver = (event) => {
 		event.preventDefault();
-	}
+	};
 	handleOnDrop(event) {
 		this.props.onDrop(event, this.props.id);
 	}
 	render() {
 		const { id, text, todos, deleteTodoFromTask, allocateDraggedTodo } = this.props;
 		const editForm = (
-			<form onSubmit={this.handleEditSubmit} className='Task-title-text'>
-				<input type='text' value={this.state.taskText} onChange={this.handleEditChange} autoFocus />
-				<button>Submit</button>
+			<form onSubmit={this.handleEditSubmit} className='Task-title-form'>
+				<input
+					type='text'
+					value={this.state.taskText}
+					onChange={this.handleEditChange}
+					autoFocus
+				/>
+				<button type='submit'><i class="fas fa-save fa-lg"></i></button>
 			</form>
 		);
-		const allTodos = todos.map((aTodo) => { // making a list of all the todos inside the Task
-			return <Todo key={aTodo.id} {...aTodo} taskId={id} deleteTodoFromTask={deleteTodoFromTask} allocateDraggedTodo={allocateDraggedTodo} />;
+		const allTodos = todos.map((aTodo) => {
+			// making a list of all the todos inside the Task
+			return (
+				<Todo
+					key={aTodo.id}
+					{...aTodo}
+					taskId={id}
+					deleteTodoFromTask={deleteTodoFromTask}
+					allocateDraggedTodo={allocateDraggedTodo}
+				/>
+			);
 		});
 		return (
 			<div className='Task' onDragOver={this.onDragOver} onDrop={this.handleOnDrop}>
@@ -72,14 +92,16 @@ class Task extends Component {
 					<div className='Task-title'>
 						<p className='Task-title-text'>{text}</p>
 						<span className='Task-options'>
-							<button onClick={this.handleTaskDelete}><i className="fas fa-trash"></i></button>
-							<button onClick={this.toggleEditForm}><i className="fas fa-pencil-alt"></i></button>
+							<button onClick={this.toggleEditForm}>
+								<i className='fas fa-pencil-alt'></i>
+							</button>
+							<button onClick={this.handleTaskDelete}>
+								<i className='fas fa-trash'></i>
+							</button>
 						</span>
 					</div>
 				)}
-				<div className='Task-container'>
-					{allTodos}
-				</div>
+				<div className='Task-container'>{allTodos}</div>
 				<form className='Task-add' onSubmit={this.handleSubmit}>
 					<textarea
 						type='text'
@@ -89,7 +111,9 @@ class Task extends Component {
 						value={this.state.newTodo}
 						onChange={this.handleChange}
 					/>
-					<button className='add-card ui-button ui-corner-all'><i className="fas fa-plus-circle"></i></button>
+					<button className='add-card ui-button ui-corner-all'>
+						<i className='fas fa-plus-circle'></i>
+					</button>
 				</form>
 			</div>
 		);
